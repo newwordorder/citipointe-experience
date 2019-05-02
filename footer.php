603d0eda557972e76d9297fbc19f4c35f497d30d
Elliot <elliot@newwordorder.com.au>
2019-05-03 08:49:48 +1000

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,13 +11,12 @@
 <footer class="footer bg--dark">
 	<div class="container">
 
-		<div class="row align-items-center">
+		<div class="row">
 
-			<div class="col-md-4 text-center">
+			<div class="col-md-4">
 				<a href="<?php echo get_home_url(); ?>" id="" class="footer__logo mb-4">
 					<img class="footer__logo" src="<?php bloginfo('template_directory'); ?>/img/logo--light.svg" alt="Logo">
 				</a>
-				
 			</div>
 			<div class="col-md-8">
 				<?php wp_nav_menu(
@@ -32,10 +31,29 @@
 				); ?>
 				
 			</div>
+
+			</div>
+			<div class="row justify-content-between">
+			<div class="col-md-6">
+
+				<p>Christian Outreach Centre trading as Citipointe Christian College, The Christian Outreach College Brisbane and Citipointe Christian College International<br>
+				CRICOS provider code: 00996F</p>
+				<p>© Copyright Citipointe Christian College | <a href="<?php echo get_home_url(); ?>/privacy-policy">Privacy Policy</a></p>
+				</div>	
+				<div class="col-md-4">
+				<p>
+				 Address: 322 Wecker Rd, Carindale<br>
+				 Brisbane Qld 4152 Australia<br>
+				 Phone: <a href="tel:+61733475899">+61 7 3347 5899</a><br>
+				 Web: <a href="http://www.citipointe.qld.edu.au">citipointe.qld.edu.au<a><br>
+				 Email: <a href="mailto:enrolments@citipointe.qld.edu.au">enrolments@citipointe.qld.edu.au</a></p>
+				
+			</div>
+		
 		</div>
 		<div class="row">
 			<div class="col text-center">
-				<p>© Copyright Citipointe Christian College<br>  <a href="<?php echo get_home_url(); ?>/privacy-policy">Privacy Policy</a></p>
+			
 			</div>
 		</div>
 
